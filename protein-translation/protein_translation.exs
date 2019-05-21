fd2f7ca6ed7879bda7f0cc2c680607b47ada61df
Edward Zhou <Edward_mjz@hotmail.com>
2019-05-21 21:06:23 +1000

    Add Protein-Translation

---
+++ protein-translation/protein_translation.exs
@@ -0,0 +1,68 @@
+defmodule ProteinTranslation do
+  @proteins %{
+    "UGU" => "Cysteine",
+    "UGC" => "Cysteine",
+    "UUA" => "Leucine",
+    "UUG" => "Leucine",
+    "AUG" => "Methionine",
+    "UUU" => "Phenylalanine",
+    "UUC" => "Phenylalanine",
+    "UCU" => "Serine",
+    "UCC" => "Serine",
+    "UCA" => "Serine",
+    "UCG" => "Serine",
+    "UGG" => "Tryptophan",
+    "UAU" => "Tyrosine",
+    "UAC" => "Tyrosine",
+    "UAA" => "STOP",
+    "UAG" => "STOP",
+    "UGA" => "STOP",
+  }
+  @doc """
+  Given an RNA string, return a list of proteins specified by codons, in order.
+  """
+  @spec of_rna(String.t()) :: {atom, list(String.t())}
+  def of_rna(rna) do
+    rna
+    |> String.split(~r/\w{3}/, include_captures: true, trim: true)
+    |> transform([])
+  end
+
+  defp transform([], acc), do: {:ok, acc}
+  defp transform([head | tail], acc) do
+    case of_codon(head) do
+      {:error, "invalid codon"} -> {:error, "invalid RNA"}
+      {:ok, "STOP"} -> {:ok, acc}
+      {:ok, protein} -> transform(tail, acc ++ [protein])
+    end
+  end
+
+  @doc """
+  Given a codon, return the corresponding protein
+
+  UGU -> Cysteine
+  UGC -> Cysteine
+  UUA -> Leucine
+  UUG -> Leucine
+  AUG -> Methionine
+  UUU -> Phenylalanine
+  UUC -> Phenylalanine
+  UCU -> Serine
+  UCC -> Serine
+  UCA -> Serine
+  UCG -> Serine
+  UGG -> Tryptophan
+  UAU -> Tyrosine
+  UAC -> Tyrosine
+  UAA -> STOP
+  UAG -> STOP
+  UGA -> STOP
+  """
+  @spec of_codon(String.t()) :: {atom, String.t()}
+  def of_codon(codon) do
+    case Map.fetch(@proteins, codon) do
+      :error -> {:error, "invalid codon"}
+      {:ok, protein} = value -> value
+    end
+  end
+end
