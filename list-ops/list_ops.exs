c3d094aecec6364d9a4d2d7389adb5077f7456e6
Edward Zhou <Edward_mjz@hotmail.com>
2019-05-21 21:46:53 +1000

    Fix TCO for list_ops

--- list-ops/list_ops.exs
+++ list-ops/list_ops.exs
@@ -6,8 +6,9 @@ defmodule ListOps do
   # `++`, `--`, `hd`, `tl`, `in`, and `length`.
 
   @spec count(list) :: non_neg_integer
-  def count([]), do: 0
-  def count([_ | tail]), do: 1 + count(tail)
+  def count(list), do: count(list, 0)
+  defp count([head | tail], acc), do: count(tail, acc + 1)
+  defp count([], acc), do: acc
 
   @spec reverse(list) :: list
   def reverse(list), do: reverse(list, [])
