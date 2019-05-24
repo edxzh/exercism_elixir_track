b343353545576632879fa4f0b53f31f87bec8f49
Edward Zhou <Edward_mjz@hotmail.com>
2019-05-24 21:08:41 +1000

    Add space-age

---
+++ space-age/space_age.exs
@@ -0,0 +1,27 @@
+defmodule SpaceAge do
+  @earth_seconds 31_557_600
+
+  @type planet ::
+          :mercury
+          | :venus
+          | :earth
+          | :mars
+          | :jupiter
+          | :saturn
+          | :uranus
+          | :neptune
+
+  @doc """
+  Return the number of years a person that has lived for 'seconds' seconds is
+  aged on 'planet'.
+  """
+  @spec age_on(planet, pos_integer) :: float
+  def age_on(:earth, seconds), do: Float.round(seconds / @earth_seconds, 3)
+  def age_on(:mercury, seconds), do: age_on(:earth, seconds) / 0.2408467
+  def age_on(:venus, seconds), do: age_on(:earth, seconds) / 0.61519726
+  def age_on(:mars, seconds), do: age_on(:earth, seconds) / 1.8808158
+  def age_on(:jupiter, seconds), do: age_on(:earth, seconds) / 11.862615
+  def age_on(:saturn, seconds), do: age_on(:earth, seconds) / 29.447498
+  def age_on(:uranus, seconds), do: age_on(:earth, seconds) / 84.016846
+  def age_on(:neptune, seconds), do: age_on(:earth, seconds) / 164.79132
+end
