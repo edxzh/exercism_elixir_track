3e288804d3ba2efdd3bbca59f96905e9b35265f5
Edward Zhou <Edward_mjz@hotmail.com>
2020-10-10 22:21:47 +1100

    Add prime-factors

---
+++ prime-factors/lib/prime_factors.ex
@@ -0,0 +1,25 @@
+defmodule PrimeFactors do
+  @doc """
+  Compute the prime factors for 'number'.
+
+  The prime factors are prime numbers that when multiplied give the desired
+  number.
+
+  The prime factors of 'number' will be ordered lowest to highest.
+  """
+  @spec factors_for(pos_integer) :: [pos_integer]
+  def factors_for(number) do
+    calc(number, 2, [])
+  end
+
+  defp calc(1, _, acc), do: Enum.reverse(acc)
+  defp calc(number, factor, acc) when number < factor * factor do
+    Enum.reverse([number | acc])
+  end
+  defp calc(number, factor, acc) when rem(number, factor) == 0 do
+    calc(div(number, factor), factor, [factor | acc])
+  end
+  defp calc(number, 2, acc), do: calc(number, 3, acc)
+  defp calc(number, factor, acc), do: calc(number, factor+2, acc)
+end
+
