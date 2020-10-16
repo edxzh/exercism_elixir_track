6562ddd1e3e801c940d1392689261da881688daa
Edward Zhou <Edward_mjz@hotmail.com>
2020-10-16 22:57:42 +1100

    Finish Sieve

--- sieve/lib/sieve.ex
+++ sieve/lib/sieve.ex
@@ -3,24 +3,47 @@ defmodule Sieve do
   Generates a list of primes up to a given limit.
   """
   @spec primes_to(non_neg_integer) :: [non_neg_integer]
-  def primes_to(limit) do
+  def primes_to(1), do: []
+  def primes_to(limit) when is_integer(limit) and limit > 1 do
+    limit
+    |> generate_primes_map
+    |> mark_map(limit)
+    |> primes_map_to_primes_list
+  end
+
+  defp generate_primes_map(limit) do
     2..limit
-    |> Enum.map(fn e -> {e, true} end)
+    |> Enum.map(fn number -> {number, true} end)
     |> Map.new
-    |> mark_primes(limit)
   end
 
-  def mark_primes(number_map, limit) do
-    square_root = :math.sqrt(limit) |> ceil()
-
-    2..square_root
-    |> Enum.each(fn p ->
-      if (Map.get(number_map, p) == true) do
-        update_number_map(number_map, p, limit)
-      end
-    end)
+  defp mark_map(primes_map, limit) do
+    2..(:math.sqrt(limit) |> floor)
+    |> Enum.reduce(primes_map, &(update_on_prime(&2, &1, limit)) )
   end
 
-  def update_number_map(number_map, p, limit) do
+  defp update_on_prime(primes_map, num, limit) do
+    case primes_map[num] do
+      true  -> mark_multiples(num, limit, primes_map)
+      false -> primes_map
+    end
+  end
+
+  defp mark_multiples(check_num, limit, primes_map) do
+    check_num..div(limit, check_num)
+    |> Enum.map(&(&1 * check_num))
+    |> Enum.reduce(primes_map, &(Map.put(&2, &1, false)))
+  end
+
+  defp primes_map_to_primes_list(primes_map) do
+    primes_map
+    |> filter_primes()
+    |> Map.keys()
+    |> Enum.sort()
+  end
+
+  defp filter_primes(primes_map) do
+    :maps.filter fn _, v -> v == true end, primes_map
   end
 end
+
