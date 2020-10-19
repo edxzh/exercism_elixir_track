a874629a376d65ff981d7208e72415d48b0619ba
Edward Zhou <Edward_mjz@hotmail.com>
2020-10-19 22:26:28 +1100

    Add simple-cipher

---
+++ simple-cipher/lib/simple_cipher.ex
@@ -0,0 +1,67 @@
+defmodule SimpleCipher do
+  @doc """
+  Given a `plaintext` and `key`, encode each character of the `plaintext` by
+  shifting it by the corresponding letter in the alphabet shifted by the number
+  of letters represented by the `key` character, repeating the `key` if it is
+  shorter than the `plaintext`.
+
+  For example, for the letter 'd', the alphabet is rotated to become:
+
+  defghijklmnopqrstuvwxyzabc
+
+  You would encode the `plaintext` by taking the current letter and mapping it
+  to the letter in the same position in this rotated alphabet.
+
+  abcdefghijklmnopqrstuvwxyz
+  defghijklmnopqrstuvwxyzabc
+
+  "a" becomes "d", "t" becomes "w", etc...
+
+  Each letter in the `plaintext` will be encoded with the alphabet of the `key`
+  character in the same position. If the `key` is shorter than the `plaintext`,
+  repeat the `key`.
+
+  Example:
+
+  plaintext = "testing"
+  key = "abc"
+
+  The key should repeat to become the same length as the text, becoming
+  "abcabca". If the key is longer than the text, only use as many letters of it
+  as are necessary.
+  """
+  def encode(plaintext, key) do
+    transform(plaintext, key, &up/2)
+  end
+
+  @doc """
+  Given a `ciphertext` and `key`, decode each character of the `ciphertext` by
+  finding the corresponding letter in the alphabet shifted by the number of
+  letters represented by the `key` character, repeating the `key` if it is
+  shorter than the `ciphertext`.
+
+  The same rules for key length and shifted alphabets apply as in `encode/2`,
+  but you will go the opposite way, so "d" becomes "a", "w" becomes "t",
+  etc..., depending on how much you shift the alphabet.
+  """
+  def decode(ciphertext, key) do
+    transform(ciphertext, key, &down/2)
+  end
+
+  defp transform(text, key, direction) do
+    key
+    |> String.to_charlist()
+    |> Stream.cycle()
+    |> Enum.zip(String.to_charlist(text))
+    |> Enum.map(& shift(&1, direction))
+    |> to_string()
+  end
+
+  defp shift({key, letter}, direction) when letter in ?a..?z do
+    ?a + Integer.mod(direction.(letter, key), 26)
+  end
+  defp shift({_, letter}, _), do: letter
+
+  defp up(letter, key), do: letter - ?a + key - ?a
+  defp down(letter, key), do: letter - key
+end
