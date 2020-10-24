722bcd4f7f46363f9ca766b23ba885600930b608
Edward Zhou <Edward_mjz@hotmail.com>
2020-10-24 21:42:59 +1100

    Add allergies

---
+++ allergies/lib/allergies.ex
@@ -0,0 +1,30 @@
+defmodule Allergies do
+  use Bitwise
+
+  @allergies %{
+    "eggs"         => 1,
+    "peanuts"      => 2,
+    "shellfish"    => 4,
+    "strawberries" => 8,
+    "tomatoes"     => 16,
+    "chocolate"    => 32,
+    "pollen"       => 64,
+    "cats"         => 128
+  }
+
+  @doc """
+  List the allergies for which the corresponding flag bit is true.
+  """
+  @spec list(non_neg_integer) :: [String.t()]
+  def list(flags) do
+    @allergies
+    |> Enum.filter(fn {_item, flag} -> (flag &&& flags) > 0 end)
+    |> Enum.map(fn {item, _flag} -> item end)
+  end
+
+  @doc """
+  Returns whether the corresponding flag bit in 'flags' is set for the item.
+  """
+  @spec allergic_to?(non_neg_integer, String.t()) :: boolean
+  def allergic_to?(flags, item), do: (flags &&& @allergies[item]) > 0
+end
