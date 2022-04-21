5b3c3347ce21d9cba8f1f639e8271d46fe4a06d9
Edward Zhou <Edward_mjz@hotmail.com>
2022-04-21 23:01:00 +1000

    Update word-count

--- word-count/lib/word_count.ex
+++ word-count/lib/word_count.ex
@@ -1,4 +1,4 @@
-defmodule Words do
+defmodule WordCount do
   @doc """
   Count the number of words in the sentence.
 
@@ -7,10 +7,13 @@ defmodule Words do
   @spec count(String.t()) :: map
   def count(sentence) do
     sentence
-    |> String.downcase
-    |> String.split(~R/[^[:alnum:]\-]/u, trim: true)
+    |> String.downcase()
+    |> String.split(~R/[^[:alnum:]\-\']/u, trim: true)
+    |> Enum.map(&String.trim(&1, "'"))
     |> Enum.reduce(%{}, fn word, word_count ->
       Map.update(word_count, word, 1, &(&1 + 1))
     end)
+
+    # or Enum.frequencies()
   end
 end
